cb6280b841468ab3baf7226e6c5931053b21f1da
efdest-star <efdest-star@users.noreply.github.com>
2026-01-31 23:59:12 +0300

    Add switch statement and conditions

---
+++ 04-Day-Conditionals/main.js
@@ -0,0 +1,21 @@
+let month = prompt ("Enter month").tolowerCase();
+switch (month) {
+    case "september":
+    case "october":
+    case "november":
+      console.log("The season is Autumn");
+             break;
+    case "december":
+    case "january":
+    case "february":
+        console.log(The season is winter");
+            break;
+   case  "march":
+   case  "april":
+   case  "may":
+         console.log("The season is spring"):
+           break;
+   default:
+      console.log("Invalid month");
+}
+
